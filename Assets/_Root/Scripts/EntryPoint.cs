5fbfd72b01ccd379ba92076f4f448957bdd00325
Net User <75517592+netuser739@users.noreply.github.com>
2023-04-07 21:47:01 +0300

    Lesson_2.2
    Добавлены IAP и ADS

--- Assets/_Root/Scripts/EntryPoint.cs
+++ Assets/_Root/Scripts/EntryPoint.cs
@@ -1,4 +1,7 @@
+using System;
 using Profile;
+using Services.Ads.UnityAds;
+using Services.IAP;
 using Tool.Analytics;
 using UnityEngine;
 
@@ -8,7 +11,9 @@ internal class EntryPoint : MonoBehaviour
     private const GameState InitialState = GameState.Start;
 
     [SerializeField] private Transform _placeForUi;
+    [SerializeField] private UnityAdsService _adsService;
     [SerializeField] private AnalyticsManager _analyticsManager;
+    [SerializeField] private IAPService _iapService;
 
     private MainController _mainController;
 
@@ -19,10 +24,22 @@ internal class EntryPoint : MonoBehaviour
         _mainController = new MainController(_placeForUi, profilePlayer);
 
         _analyticsManager.SendMainMenuOpenedEvent();
+
+        if (_adsService.IsInitialized) OnAdsInitialized();
+        else _adsService.Initialized.AddListener(OnAdsInitialized);
+
+        if (_iapService.IsInitialized) OnIapInitialized();
+        else _iapService.Initialized.AddListener(OnIapInitialized);
     }
 
     private void OnDestroy()
     {
+        _adsService.Initialized.RemoveListener(OnAdsInitialized);
+        _iapService.Initialized.RemoveListener(OnIapInitialized);
         _mainController.Dispose();
     }
+
+    private void OnAdsInitialized() => _adsService.InterstitialPlayer.Play();
+    private void OnIapInitialized() => _iapService.Buy("prod_1");
+
 }
